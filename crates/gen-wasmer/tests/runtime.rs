17c9c7b6840d38c92eef61fd3d05d214ea682c86
kajacx <kajacx@gmail.com>
2023-05-14 15:38:16 +0200

    It looks like we are finally getting a real error

--- crates/gen-wasmer/tests/runtime.rs
+++ crates/gen-wasmer/tests/runtime.rs
@@ -13,7 +13,39 @@ pub fn instantiate<T, I>(
 where
     I: FnOnce(&Instance, &dyn wasmer::AsStoreRef) -> Result<(), anyhow::Error>,
 {
-    let module = Module::from_file(&*store, wasm)?;
+    // let module = Module::from_file(&*store, wasm)?;
+    // let bytes = std::fs::read(wasm)?;
+    // panic!("WHAT IS IT? {:?}", wasm);
+
+    // let mut bytes = Vec::<u8>::new();
+    // let mut escaped = false;
+    // let mut iter = wasm.bytes();
+    // while let Some(b) = iter.next() {
+    //     if escaped {
+    //         match b {
+    //             b'x' => {
+    //                 let mut hex = String::new();
+    //                 hex.push(iter.next().unwrap() as char);
+    //                 hex.push(iter.next().unwrap() as char);
+    //                 let byte = u8::from_str_radix(&hex, 16).unwrap();
+    //                 bytes.push(byte);
+    //             }
+    //             _ => panic!("Invalid escape sequence: {:?}", b),
+    //         }
+    //         escaped = false;
+    //     } else if b == b'\\' {
+    //         escaped = true;
+    //     } else {
+    //         bytes.push(b);
+    //     }
+    // }
+
+    let mut bytes = Vec::<u8>::with_capacity(wasm.len() / 2);
+    for index in (0..wasm.len()/2).map(|x| x * 2) {
+        bytes.push(u8::from_str_radix(&wasm[index..index+2], 16).unwrap());
+    }
+
+    let module = Module::from_binary(&*store, &bytes)?;
 
     let wasi_env = WasiState::new("test").finalize(store)?;
     let mut imports = wasi_env
@@ -33,7 +65,8 @@ where
 }
 
 // crates/gen-wasmer$ wasm-pack test --node --features js
-#[wasm_bindgen_test::wasm_bindgen_test]
+#[cfg_attr(feature = "js", wasm_bindgen_test::wasm_bindgen_test)]
 fn should_fail() {
-    panic!()
+    // panic!()
+    numbers_rust::test().unwrap()
 }
